4ecbab71c65ceea5d47e238911e5126ed02c1a64
tarunc16 <114050999+tarunc16@users.noreply.github.com>
2022-10-12 10:55:55 +1100

    Add files via upload

---
+++ Assignment2/vega_lite_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "Vis1Map.json";
+vegaEmbed('#map', vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Vis2.vega";
+vegaEmbed('#bubble_chart', vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "Vis3_Aus_Energy.vega";
+vegaEmbed('#bar_and_line', vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
