e2fc8297526c08b631f51a6afb4a1668518d0c88
tarunc16 <114050999+tarunc16@users.noreply.github.com>
2022-10-12 10:58:36 +1100

    Update vega_lite_vis.js

--- Assignment2/vega_lite_vis.js
+++ Assignment2/vega_lite_vis.js
@@ -8,7 +8,7 @@ vegaEmbed('#bubble_chart', vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "Vis3_Aus_Energy.vega";
+var vg_3 = "Vis3_Aus_Energy.json";
 vegaEmbed('#bar_and_line', vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
