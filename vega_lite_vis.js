0a98691214cbdc1b9ac2d1adc87459d7ff5ed1be
tarunc16 <114050999+tarunc16@users.noreply.github.com>
2022-10-12 16:45:23 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,14 @@
 var vg_1 = "Vis1Map.json";
-vegaEmbed('#Map', vg_1).then(function(result) {
+vegaEmbed('#map', vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "Vis2.vega";
-vegaEmbed('#Bubble', vg_2).then(function(result) {
+vegaEmbed('#bubble_chart', vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "Vis3_Aus_Energy.json";
+vegaEmbed('#bar_and_line', vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
