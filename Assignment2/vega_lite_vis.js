07343393ad62b2375317a01e3a1fcbac491fea57
tarunc16 <tcho0021@student.monash.edu>
2022-10-15 23:29:03 +1100

    update

--- Assignment2/vega_lite_vis.js
+++ Assignment2/vega_lite_vis.js
@@ -12,3 +12,13 @@ var vg_3 = "Vis3_Aus_Energy.json";
 vegaEmbed('#bar_and_line', vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "Vis4_select_countries_renewables.vega";
+vegaEmbed('#line', vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "Vis5_heatmap.vega";
+vegaEmbed('#heatmap', vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
